8b57871a984e89a9ea645d4049255ac66bd1228d
misato.uematsu <misato.uematsu@gpol.co.jp>
2019-08-16 17:15:11 +0900

    [modify] PWA修正

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-const divInstall = document.getElementById('installContainer');
-const butInstall = document.getElementById('butInstall');
-
-/* Put code here */
-
-
-
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js');
