00050f5ca87e83dcaee6fb4e6461f48d46f345f4
misato.uematsu <misato.uematsu@gpol.co.jp>
2019-08-16 18:10:46 +0900

    [modify] PWA修正

--- service-worker.js
+++ service-worker.js
@@ -1,76 +1,11 @@
-const CACHE_NAME = 'cache-v1';
-const urlsToCache = [
-    '/',
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-              .then((cache) => {
-                  // 指定されたリソースをキャッシュに追加する
-                  return cache.addAll(urlsToCache);
-              })
-    );
+// service-worker.js
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
 });
 
-
-self.addEventListener('activate', (event) => {
-  var cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-      caches.keys().then((cacheNames) => {
-          return Promise.all(
-              cacheNames.map((cacheName) => {
-                  // ホワイトリストにないキャッシュ(古いキャッシュ)は削除する
-                  if (cacheWhitelist.indexOf(cacheName) === -1) {
-                      return caches.delete(cacheName);
-                  }
-              })
-          );
-      })
-  );
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-      caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-
-                // 重要：リクエストを clone する。リクエストは Stream なので
-                // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
-                // 必要なので、リクエストは clone しないといけない
-                let fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest)
-                    .then((response) => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // 重要：レスポンスを clone する。レスポンスは Stream で
-                        // ブラウザ用とキャッシュ用の2回必要。なので clone して
-                        // 2つの Stream があるようにする
-                        let responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                              .then((cache) => {
-                                  cache.put(event.request, responseToCache);
-                              });
-
-                        return response;
-                    });
-            })
-  );
-});
-
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  alert('add to');
-});
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+self.addEventListener('fetch', function(event) {});
