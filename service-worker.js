6a7313d74b2ea60dc9caa0ab95cd3cc3373a6b1c
misato10 <midori692227@gmail.com>
2019-08-18 14:43:04 +0900

    [modify] PWA修正

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,58 @@
-self.addEventListener('fetch', function(event) {});
+var CACHE_NAME = '1';
+var urlsToCache = [
+  /*'/',
+  '/index.html',
+  '/bundle.js',
+  '/material.min.css',
+  '/material.min.js',
+  '/favicon.ico'*/
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
+        })
+    );
+  });
+   
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // キャッシュがあったのでそのレスポンスを返す
+          if (response) {
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+              function(response) {
+                // レスポンスが正しいかをチェック
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                  return response;
+                }
+            
+                // 重要：レスポンスを clone する。レスポンスは Stream で
+                // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                // 2つの Stream があるようにする
+                var responseToCache = response.clone();
+            
+                caches.open(CACHE_NAME)
+                  .then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                  });
+            
+                return response;
+              }
+            );
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
+
+
 
 
