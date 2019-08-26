544de3e079ac6f2ada8d6d45ea0f7881289285b3
misato.uematsu <misato.uematsu@gpol.co.jp>
2019-08-26 19:34:29 +0900

    [modify] PWA修正

---
+++ js/pwa_.js
@@ -0,0 +1,32 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+  
+  // Code to handle install prompt on desktop
+  
+  let deferredPrompt;
+  const addArea = document.querySelector('.addArea');
+  const addBtn = document.querySelector('.addButton');
+  const closeBtn = document.querySelector('.closeButton');
+  addArea.style.display = 'none';
+  
+  window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen
+    addArea.style.display = 'block';
+  
+    addBtn.addEventListener('click', (e) => {
+      // hide our user interface that shows our A2HS button
+      aaddArea.style.display = 'none';
+      // Show the prompt
+      
+    closeBtn.addEventListener('click', (e) => {
+      aaddArea.style.display = 'none';
+    });
+    
+  });
